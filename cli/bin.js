d8919ba24f389a22a136ff74902a1d680ed491cf
Ruby Allison Rose <M3TIOR@users.noreply.github.com>
2020-11-22 19:50:07 -0800

    chore: Added a linter (ESLint) for syntax standards control.
    Also changed the indentation type to tabs since that saves
    bytes in transit, and execution time. Another bonus,
    easily configurable editor indentation. Different people can
    have different indentation sizes based on their preference,
    since tab size rendering is up to the editor. (I like tabs)

--- cli/bin.js
+++ cli/bin.js
@@ -1,77 +1,72 @@
 #!/usr/bin/env node
 /**
  * @author Isaac Z. Schlueter and Contributors
- * @license ISC License
- *   <strong>Copyright (c) Isaac Z. Schlueter and Contributors</strong>
- *   <p>
- *     See the LICENSE file in the root of this repository for permissions and
- *     restrictions.
- *   </p>
+ * @license ISC
  */
 
-const rimraf = require('../src/rimraf.js')
+const rimraf = require('../src/rimraf.js');
 
-const path = require('path')
+const path = require('path');
 
-const isRoot = arg => /^(\/|[a-zA-Z]:\\)$/.test(path.resolve(arg))
+const isRoot = arg => /^(\/|[a-zA-Z]:\\)$/.test(path.resolve(arg));
 const filterOutRoot = arg => {
-  const ok = preserveRoot === false || !isRoot(arg)
-  if (!ok) {
-    console.error(`refusing to remove ${arg}`)
-    console.error('Set --no-preserve-root to allow this')
-  }
-  return ok
-}
+	const ok = preserveRoot === false || !isRoot(arg);
+	if (!ok) {
+		console.error(`refusing to remove ${arg}`);
+		console.error('Set --no-preserve-root to allow this');
+	}
+	return ok;
+};
 
-let help = false
-let dashdash = false
-let noglob = false
-let preserveRoot = true
+let help = false;
+let dashdash = false;
+let noglob = false;
+let preserveRoot = true;
 const args = process.argv.slice(2).filter(arg => {
-  if (dashdash)
-    return !!arg
-  else if (arg === '--')
-    dashdash = true
-  else if (arg === '--no-glob' || arg === '-G')
-    noglob = true
-  else if (arg === '--glob' || arg === '-g')
-    noglob = false
-  else if (arg.match(/^(-+|\/)(h(elp)?|\?)$/))
-    help = true
-  else if (arg === '--preserve-root')
-    preserveRoot = true
-  else if (arg === '--no-preserve-root')
-    preserveRoot = false
-  else
-    return !!arg
-}).filter(arg => !preserveRoot || filterOutRoot(arg))
+	if (dashdash)
+		return !!arg;
+	else if (arg === '--')
+		dashdash = true;
+	else if (arg === '--no-glob' || arg === '-G')
+		noglob = true;
+	else if (arg === '--glob' || arg === '-g')
+		noglob = false;
+	else if (arg.match(/^(-+|\/)(h(elp)?|\?)$/))
+		help = true;
+	else if (arg === '--preserve-root')
+		preserveRoot = true;
+	else if (arg === '--no-preserve-root')
+		preserveRoot = false;
+	else
+		return !!arg;
+}).filter(arg => !preserveRoot || filterOutRoot(arg));
 
 const go = n => {
-  if (n >= args.length)
-    return
-  const options = noglob ? { glob: false } : {}
-  rimraf(args[n], options, er => {
-    if (er)
-      throw er
-    go(n+1)
-  })
-}
+	if (n >= args.length)
+		return;
+	const options = noglob ? { glob: false } : {};
+	rimraf(args[n], options, er => {
+		if (er)
+			throw er;
+		go(n+1);
+	});
+};
 
 if (help || args.length === 0) {
-  // If they didn't ask for help, then this is not a "success"
-  const log = help ? console.log : console.error
-  log('Usage: rimraf <path> [<path> ...]')
-  log('')
-  log('  Deletes all files and folders at "path" recursively.')
-  log('')
-  log('Options:')
-  log('')
-  log('  -h, --help          Display this usage info')
-  log('  -G, --no-glob       Do not expand glob patterns in arguments')
-  log('  -g, --glob          Expand glob patterns in arguments (default)')
-  log('  --preserve-root     Do not remove \'/\' (default)')
-  log('  --no-preserve-root  Do not treat \'/\' specially')
-  log('  --                  Stop parsing flags')
-  process.exit(help ? 0 : 1)
+	// If they didn't ask for help, then this is not a "success"
+	const log = help ? console.log : console.error;
+	log('Usage: rimraf <path> [<path> ...]');
+	log('');
+	log('  Deletes all files and folders at "path" recursively.');
+	log('');
+	log('Options:');
+	log('');
+	log('  -h, --help          Display this usage info');
+	log('  -G, --no-glob       Do not expand glob patterns in arguments');
+	log('  -g, --glob          Expand glob patterns in arguments (default)');
+	log('  --preserve-root     Do not remove \'/\' (default)');
+	log('  --no-preserve-root  Do not treat \'/\' specially');
+	log('  --                  Stop parsing flags');
+	process.exit(help ? 0 : 1);
 } else
-  go(0)
+	go(0);
--- test/basic.js
+++ test/basic.js
@@ -1,116 +1,111 @@
 /**
  * @author Isaac Z. Schlueter and Contributors
- * @license ISC License
- *   <strong>Copyright (c) Isaac Z. Schlueter and Contributors</strong>
- *   <p>
- *     See the LICENSE file in the root of this repository for permissions and
- *     restrictions.
- *   </p>
+ * @license ISC
  */
 
-var rimraf = require('../src/rimraf.js')
-var t = require('tap')
+var rimraf = require('../src/rimraf.js');
+var t = require('tap');
 
-var fs = require('fs')
-var fill = require('./fill.js')
+var fs = require('fs');
+var fill = require('./fill.js');
 
 t.test('initial clean', function (t) {
-  rimraf.sync(__dirname + '/target')
-  t.throws(function () {
-    fs.statSync(__dirname + '/target')
-  })
-  t.end()
-})
+	rimraf.sync(__dirname + '/target');
+	t.throws(function () {
+		fs.statSync(__dirname + '/target');
+	});
+	t.end();
+});
 
 t.test('sync removal', function (t) {
-  fill()
-  t.ok(fs.statSync(__dirname + '/target').isDirectory())
+	fill();
+	t.ok(fs.statSync(__dirname + '/target').isDirectory());
 
-  rimraf.sync(__dirname + '/target')
-  t.throws(function () {
-    fs.statSync(__dirname + '/target')
-  })
-  t.end()
-})
+	rimraf.sync(__dirname + '/target');
+	t.throws(function () {
+		fs.statSync(__dirname + '/target');
+	});
+	t.end();
+});
 
 t.test('async removal', function (t) {
-  fill()
-  t.ok(fs.statSync(__dirname + '/target').isDirectory())
+	fill();
+	t.ok(fs.statSync(__dirname + '/target').isDirectory());
 
-  rimraf(__dirname + '/target', function (er) {
-    if (er)
-      throw er
-    t.throws(function () {
-      fs.statSync(__dirname + '/target')
-    })
-    t.end()
-  })
-})
+	rimraf(__dirname + '/target', function (er) {
+		if (er)
+			throw er;
+		t.throws(function () {
+			fs.statSync(__dirname + '/target');
+		});
+		t.end();
+	});
+});
 
 t.test('glob', function (t) {
-  t.plan(2)
-  t.test('async', function (t) {
-    fill()
-    var glob = require('glob')
-    var pattern = __dirname + '/target/f-*'
-    var before = glob.sync(pattern)
-    t.notEqual(before.length, 0)
-    rimraf(pattern, function (er) {
-      if (er)
-        throw er
-      var after = glob.sync(pattern)
-      t.same(after, [])
-      rimraf.sync(__dirname + '/target')
-      t.end()
-    })
-  })
-  t.test('sync', function (t) {
-    fill()
-    var glob = require('glob')
-    var pattern = __dirname + '/target/f-*'
-    var before = glob.sync(pattern)
-    t.notEqual(before.length, 0)
-    rimraf.sync(pattern)
-    var after = glob.sync(pattern)
-    t.same(after, [])
-    rimraf.sync(__dirname + '/target')
-    t.end()
-  })
-})
+	t.plan(2);
+	t.test('async', function (t) {
+		fill();
+		var glob = require('glob');
+		var pattern = __dirname + '/target/f-*';
+		var before = glob.sync(pattern);
+		t.notEqual(before.length, 0);
+		rimraf(pattern, function (er) {
+			if (er)
+				throw er;
+			var after = glob.sync(pattern);
+			t.same(after, []);
+			rimraf.sync(__dirname + '/target');
+			t.end();
+		});
+	});
+	t.test('sync', function (t) {
+		fill();
+		var glob = require('glob');
+		var pattern = __dirname + '/target/f-*';
+		var before = glob.sync(pattern);
+		t.notEqual(before.length, 0);
+		rimraf.sync(pattern);
+		var after = glob.sync(pattern);
+		t.same(after, []);
+		rimraf.sync(__dirname + '/target');
+		t.end();
+	});
+});
 
 t.test('no glob', function (t) {
-  t.plan(2)
-  t.test('async', function (t) {
-    fill()
-    var glob = require('glob')
-    var pattern = __dirname + '/target/f-*'
-    var before = glob.sync(pattern)
-    t.notEqual(before.length, 0)
-    rimraf(pattern, { disableGlob: true }, function (er) {
-      if (er)
-        throw er
-      var after = glob.sync(pattern)
-      t.same(after, before)
-      rimraf.sync(__dirname + '/target')
-      t.end()
-    })
-  })
-  t.test('sync', function (t) {
-    fill()
-    var glob = require('glob')
-    var pattern = __dirname + '/target/f-*'
-    var before = glob.sync(pattern)
-    t.notEqual(before.length, 0)
-    rimraf.sync(pattern, { disableGlob: true })
-    var after = glob.sync(pattern)
-    t.same(after, before)
-    rimraf.sync(__dirname + '/target')
-    t.end()
-  })
-})
+	t.plan(2);
+	t.test('async', function (t) {
+		fill();
+		var glob = require('glob');
+		var pattern = __dirname + '/target/f-*';
+		var before = glob.sync(pattern);
+		t.notEqual(before.length, 0);
+		rimraf(pattern, { disableGlob: true }, function (er) {
+			if (er)
+				throw er;
+			var after = glob.sync(pattern);
+			t.same(after, before);
+			rimraf.sync(__dirname + '/target');
+			t.end();
+		});
+	});
+	t.test('sync', function (t) {
+		fill();
+		var glob = require('glob');
+		var pattern = __dirname + '/target/f-*';
+		var before = glob.sync(pattern);
+		t.notEqual(before.length, 0);
+		rimraf.sync(pattern, { disableGlob: true });
+		var after = glob.sync(pattern);
+		t.same(after, before);
+		rimraf.sync(__dirname + '/target');
+		t.end();
+	});
+});
 
 t.test('verify that cleanup happened', function (t) {
-  t.throws(fs.statSync.bind(fs, __dirname + '/../target'))
-  t.throws(fs.statSync.bind(fs, __dirname + '/target'))
-  t.end()
-})
+	t.throws(fs.statSync.bind(fs, __dirname + '/../target'));
+	t.throws(fs.statSync.bind(fs, __dirname + '/target'));
+	t.end();
+});
